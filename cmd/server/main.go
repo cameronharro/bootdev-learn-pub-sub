57ec7bfdc0135e52c1a613c54ef2edf454940d2a
cameronharro <103271364+cameronharro@users.noreply.github.com>
2026-02-21 14:24:00 -0600

    feat: Lesson 3.1

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -5,18 +5,35 @@ import (
 	"os"
 	"os/signal"
 
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
 func main() {
-	connStr := "amqp://guest:guest@localhost:5672/"
+	conn, _ := createConn("amqp://guest:guest@localhost:5672/")
+	defer conn.Close()
+	ch, err := conn.Channel()
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
+	err = pubsub.PublishJSON(ch, string(routing.ExchangePerilDirect), string(routing.PauseKey), routing.PlayingState{IsPaused: true})
+	waitKill()
+}
+
+func createConn(connStr string) (*amqp.Connection, error) {
 	conn, err := amqp.Dial(connStr)
 	if err != nil {
 		fmt.Printf(err.Error())
+		os.Exit(1)
 	}
-	defer conn.Close()
 	fmt.Println("Started Peril server")
+	return conn, nil
+}
 
+func waitKill() {
 	signals := make(chan os.Signal, 1)
 	signal.Notify(signals, os.Interrupt)
 	sig := <-signals
