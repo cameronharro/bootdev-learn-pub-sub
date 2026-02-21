a9bac4e6be280362f4aea76c3ebaf32e54ab6aad
cameronharro <103271364+cameronharro@users.noreply.github.com>
2026-02-21 15:02:40 -0600

    feat: lesson 3.5

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,7 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/signal"
+
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
-	fmt.Println("Starting Peril client...")
+	conn, _ := createConn("amqp://guest:guest@localhost:5672/")
+	username, err := gamelogic.ClientWelcome()
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+	defer conn.Close()
+
+	_, _, err = DeclareAndBind(conn,
+		routing.ExchangePerilDirect,
+		fmt.Sprintf("pause.%s", username),
+		routing.PauseKey,
+		"transient",
+	)
+
+	waitKill()
+}
+
+func createConn(connStr string) (*amqp.Connection, error) {
+	conn, err := amqp.Dial(connStr)
+	if err != nil {
+		fmt.Printf(err.Error())
+		os.Exit(1)
+	}
+	fmt.Println("Started Peril client")
+	return conn, nil
+}
+
+func waitKill() {
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, os.Interrupt)
+	sig := <-signals
+	fmt.Printf("Signal %v caught: shutting down...", sig)
 }
